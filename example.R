f3d0432bf674b036242e384788bb54ccb5956201
George Vega Yon <g.vegayon@gmail.com>
2012-01-31 16:08:03 -0300

    Test script

---
+++ example.R
@@ -0,0 +1,17 @@
+require(XML)
+require(xlsx)
+
+source('pde.r', encoding='UTF-8')
+
+pde(
+  path=getwd(),
+  output=getwd(),
+  replace=T,
+  timeFormat='yyyy',
+  lang=c('en','es'),
+  name=,
+  description=,
+  url=,
+  providerName=,
+  providerURL=,
+  extension=)
