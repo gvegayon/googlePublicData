24a921ef3b1aaf962bcae4522f178faeaeaef94a
George Vega Yon <g.vegayon@gmail.com>
2012-02-15 16:49:35 -0300

    Improving the example

--- example.R
+++ example.R
@@ -1,13 +1,29 @@
 require(XML)
 require(xlsx)
-#setwd('c:/comandos_paquetes_librerias/r/rdspl/')
-source('pde.r', encoding='UTF-8')
 
+directory <- 'c:/comandos_paquetes_librerias/r/rdspl/series_sc'
+funsource <- 'c:/comandos_paquetes_librerias/r/rdspl/pde.r'
+source(funsource, encoding='UTF-8')
 pde <- compiler::cmpfun(pde)
- 
+
+# Basic DSPL
 mydspl <- pde(
-  path='c:/Users/George/Documents/Programacion/rdspl/series_sc/',
-  output='c:/Users/George/Documents/Programacion/rdspl/series_sc/',
+  path=directory,
+  timeFormat='yyyy-MM',
+  lang='en',
+  name='Unemployment Insurance',
+  description='Some unemployment insurance statistics',
+  extension='xls'
+)
+
+cat(mydspl2$xml) # Prints the XML file
+
+View(mydspl2$variables) # Views the variables
+
+# Output of the complete bundle adding some more info
+mydspl <- pde(
+  path=directory,
+  output=directory,
   replace=T,
   timeFormat='yyyy-MM',
   lang=c('en','es'),
@@ -17,13 +33,19 @@ mydspl <- pde(
   providerName=c('Chilean Pension Supervisor','Superintendencia de Pensiones, Chile'),
   providerURL='http://www.spensiones.cl/',
   extension='xls',
-  encoding='UTF-8')
+  encoding='UTF-8'
+)
 
-mydspl
+mydspl # final message
 
-mydspl2 <- pde(
-  path='c:/comandos_paquetes_librerias/r/rdspl/series_sc/',
-  output=NA,
+# Generating a descriptive TAB file for the concepts adding topics and descriptions
+genMoreInfo(directory, encoding='UTF-8', ext='xls', 
+            output=paste(directory,'/config.tab',sep=''))
+
+# Output of the complete bundle adding a descriptive file (moreinfo)
+mydspl <- pde(
+  path=directory,
+  output=directory,
   replace=T,
   timeFormat='yyyy-MM',
   lang=c('en','es'),
@@ -33,8 +55,5 @@ mydspl2 <- pde(
   providerName=c('Chilean Pension Supervisor','Superintendencia de Pensiones, Chile'),
   providerURL='http://www.spensiones.cl/',
   extension='xls',
-  encoding='UTF-8')
-
-cat(mydspl2$xml)
-
-View(mydspl2$variables)
+  encoding='UTF-8',
+  moreinfo=paste(directory,'config.tab',sep='/'))
