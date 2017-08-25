7cf09c9e5fda9fa68a3162b28347f6497bb76b6c
George G. Vega Yon <g.vegayon@gmail.com>
2017-08-24 22:49:08 -0700

    Fixing test

--- tests/testthat/test-dspl.r
+++ tests/testthat/test-dspl.r
@@ -1,6 +1,24 @@
 context("Main function")
 
 test_that("dspl", {
+  # Loading datasets
+  
+  data(countries)
+  data(country_slice)
+  data(gender_country_slice)
+  data(genders)
+  data(state_slice)
+  data(states)
+  
+  path <- tempdir()
+  
+  write.table(countries, file = paste0(path,"/countries.tab"), sep = "\t")
+  write.table(country_slice, file = paste0(path,"/country_slice.tab"), sep = "\t")
+  write.table(gender_country_slice, file = paste0(path,"/gender_country_slice.tab"), sep = "\t")
+  write.table(genders, file = paste0(path,"/genders.tab"), sep = "\t")
+  write.table(state_slice, file = paste0(path,"/state_slice.tab"), sep = "\t")
+  write.table(states, file = paste0(path,"/states.tab"), sep = "\t")
+  
   path <- base::system.file("data", package="googlePublicData")
   suppressMessages(dat <- dspl(path))
   expect_output(print(dat), "<dspl")
